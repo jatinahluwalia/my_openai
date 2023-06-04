1a1a502554c6c36abb4bbb73fa06c28c89b2d614
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2023-06-04 21:28:14 +0530

    loader

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 "use client"
 
 import { useState } from "react"
+import Loader from "@/components/Loader/Loader"
 
 export default function Home() {
+  const [loading, setLoading] = useState(false)
   const [description, setDescription] = useState('')
   const handleSubmit = async (e) => {
+    setLoading(true)
     e.preventDefault()
     const res = await fetch('/api/v2/openai', {
       method: 'POST',
@@ -12,6 +15,7 @@ export default function Home() {
     })
     const data = await res.json()
     setDescription(data.description)
+    setLoading(false)
   }
   return (
     <form onSubmit={handleSubmit} className="flex flex-col mx-auto gap-4 text-3xl h-screen p-5">
@@ -20,6 +24,7 @@ export default function Home() {
         <label htmlFor="prompt" className="mb-5">Type here:</label>
         <input type="text" placeholder="Type anything..." name="prompt" id="prompt" className="shadow-md px-4 py-2 rounded-full" />
       </div>
+      {loading && <Loader />}
       {description && <div className="text-2xl">
         <p>Hello, I am the AI made by Jatin Ahluwalia!</p>
         <p className="text-gray-600">{description}</p>
---
+++ components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import './loader.css'
+
+const Loader = () => {
+    return (
+        <div class="typewriter">
+            <div class="slide"><i></i></div>
+            <div class="paper"></div>
+            <div class="keyboard"></div>
+        </div>
+    )
+}
+
+export default Loader
